eada857a68b93eee42838529815068a5d4ac0362
TimFix <sawaoran@mail.ru>
2022-11-26 00:37:18 +0300

    7 update

---
+++ bin/brain-gcd.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import { playerName } from '../src/cli.js';
+import { gameOver, getRandom } from '../src/index.js';
+
+const name = playerName(); 
+console.log('Find the greatest common divisor of given numbers.');
+for (let i=0 ; i < 3 ; i++) {
+    let a = getRandom();
+    let b = getRandom();
+    let rightAnswer = 0;
+    let lowNumber = 0;
+    if (a > b) lowNumber = b;
+    else lowNumber = a;
+    for (let j = lowNumber; j > 0; j--){
+        if (((a % j) === 0) && ((b % j) === 0)) {
+            rightAnswer = j;
+            break
+        }
+    }
+    console.log("Question:", a, b);
+    const answer = readlineSync.question("Your answer: ");
+    let answerNumber = Number(answer);
+    if (answerNumber === rightAnswer) console.log('Correct!');
+    else {
+        gameOver(name, answer, rightAnswer);
+        break;
+    }
+    if (i === 2) console.log("Congratulations,", name);
+}
