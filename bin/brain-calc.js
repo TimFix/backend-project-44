c73b251707ead055461399c4f1c415fe86f4c13d
TimFix <110151346+TimFix@users.noreply.github.com>
2023-08-02 12:50:42 +0300

    Add files via upload

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import { gameCalc } from '../src/games/calc.js';
 
 gameCalc();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import { playerName } from '../src/cli.js';
 
-const name = playerName(); 
+playerName(); 
